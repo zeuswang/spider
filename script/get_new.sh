e6c3585553c72c1d7c06e3c9af330bc9f707cedd
zeuswoody <zeuswoody@qq.com>
2015-04-06 11:07:00 +0800

    bywangwei

--- script/get_new.sh
+++ script/get_new.sh
@@ -3,5 +3,5 @@ python get_new_movie.py ../conf/template ../data/movie.$date ../data/link.$date 
 cp ../data/movie.$date ../data/movie
 cp ../data/link.$date ../data/link
 
-cd /home/wangwei/moviesite
-python db/update.py |tee ../log/update.$date
+cd /home/wangwei/moviesite/moviesite/
+python db/update.py |tee update.log
